749e4e37e1ef47a35d512ad4c30fe2ea3d754ae2
Ian Cooperman <34320199+iancooperman@users.noreply.github.com>
2024-06-01 17:46:08 -0700

    Added boilerplate for fastapi and react apps

---
+++ frontend/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>LAUSD Password Generator</h1>
+      <p>{randomPassword()}</p>
+    </div>
+  );
+}
+
+
+
+function randomAdjective() {
+  return "Fluffy";
+}
+
+function randomNoun() {
+  return "Hat";
+}
+
+function randomDigit() {
+  return '1';
+}
+
+function randomSymbol() {
+  return '!';
+}
+
+function randomPassword() {
+  return randomAdjective() + randomNoun() + randomDigit() + randomSymbol();
+}
+
+
+
+export default App;
