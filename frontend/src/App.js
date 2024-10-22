5f12bd4e0ad1791d11cdb3bf09ace6dd87000368
Ian Cooperman <34320199+iancooperman@users.noreply.github.com>
2024-10-21 20:34:40 -0700

    Removed an unflattering word

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -80,7 +80,7 @@ function Content() {
 function About() {
 
   let paragraphs = [
-    "SaludSecure was born out of a desire to lend a helping hand in simplifying password creation, amidst the increasingly complex requirements imposed by a certain unesteemed, school-adjacent organization. As soon as the requirements were unveiled, the author of this site knew they might be a bit of a stretch for his non-IT peers to master.",
+    "SaludSecure was born out of a desire to lend a helping hand in simplifying password creation, amidst the increasingly complex requirements imposed by a certain school-adjacent organization. As soon as the requirements were unveiled, the author of this site knew they might be a bit of a stretch for his non-IT peers to master.",
     'The requirement that new passwords must not be "common" left many feeling anxious, unsure about what to do after their otherwise good-enough password was rejected for being too "common", and asking for a simpler solution.',
     "SaludSecure saves the day by generating passwords that are guaranteed* to not result in any red Xs, while still being straightforward enough to remember. And, with a bit of luck, even the most digitally-challenged individuals can use them with ease."
   ];
